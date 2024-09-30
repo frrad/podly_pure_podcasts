3dc62c1d773246bccacac04fd5d971354e6accab
Frederick Robinson <frederick@rhino.localdomain>
2024-09-29 21:39:00 -0700

    pytest

---
+++ src/tests/test_transcribe.py
@@ -0,0 +1,18 @@
+import logging
+
+from openai import OpenAI
+
+from podcast_processor.env_settings import populate_env_settings
+from podcast_processor.transcribe import RemoteWhisperTranscriber
+
+
+def test_transcribe():
+    logger = logging.getLogger("global_logger")
+    env_settings = populate_env_settings()
+    client = OpenAI(
+        base_url=env_settings.openai_base_url,
+        api_key=env_settings.openai_api_key,
+    )
+
+    transcriber = RemoteWhisperTranscriber(logger, client)
+    transcriber.transcribe()
