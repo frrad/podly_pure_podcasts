a40fc4b1c47929b1b54de7fdbf6d8ba6ea8c7fda
Frederick Robinson <frederick@rhino.localdomain>
2024-09-29 23:36:17 -0700

    minor cleanup

--- src/podcast_processor/transcribe.py
+++ src/podcast_processor/transcribe.py
@@ -3,7 +3,7 @@ import math
 import os
 import shutil
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Tuple
 
 from openai import OpenAI
 from openai.types.audio.transcription_segment import TranscriptionSegment
@@ -21,37 +21,61 @@ class RemoteWhisperTranscriber(Transcriber):
         self.logger = logger
         self.openai_client = openai_client
 
-    def transcribe(self, audio_path: str) -> List[TranscriptionSegment]:
+    def transcribe(self, audio_file_path: str) -> List[TranscriptionSegment]:
         self.logger.info("Using remote whisper")
-        self.split_file(audio_path)
+        audio_chunk_path = audio_file_path + "_parts"
+
+        chunks = self.split_file(audio_file_path, audio_chunk_path)
+
         all_segments = []
-        for i in range(0, len(os.listdir(f"{audio_path}_parts")), 1):
-            segments = self.get_segments_for_chunk(f"{audio_path}_parts/{i}.mp3")
-            all_segments.extend(segments)
-        # clean up
-        shutil.rmtree(f"{audio_path}_parts")
+
+        for chunk in chunks:
+            chunk_path, offset = chunk
+            segments = self.get_segments_for_chunk(chunk_path)
+            all_segments.extend(self.add_offset_to_segments(segments, offset))
+
+        shutil.rmtree(audio_chunk_path)
         return all_segments
 
+    @staticmethod
+    def add_offset_to_segments(
+        segments: List[TranscriptionSegment], offset_ms: int
+    ) -> List[TranscriptionSegment]:
+        offset_sec = float(offset_ms) / 1000.0
+        for segment in segments:
+            segment.start += offset_sec
+            segment.end += offset_sec
+
+        return segments
+
     def split_file(
-        self, audio_path: str, chunk_size_bytes: int = 24 * 1024 * 1024
-    ) -> None:
-        if not os.path.exists(audio_path + "_parts"):
-            os.makedirs(audio_path + "_parts")
-        audio = AudioSegment.from_mp3(audio_path)
-        duration_milliseconds = len(audio)
-        chunk_duration = (
-            chunk_size_bytes / os.path.getsize(audio_path)
-        ) * duration_milliseconds
-        chunk_duration = int(chunk_duration)
+        self,
+        audio_file_path: str,
+        audio_chunk_path: str,
+        chunk_size_bytes: int = 24 * 1024 * 1024,
+    ) -> List[Tuple[str, int]]:
 
-        num_chunks = math.ceil(duration_milliseconds / chunk_duration)
+        if not os.path.exists(audio_chunk_path):
+            os.makedirs(audio_chunk_path)
+        audio = AudioSegment.from_mp3(audio_file_path)
+        duration_ms = len(audio)
+        chunk_duration_ms = (
+            chunk_size_bytes / os.path.getsize(audio_file_path)
+        ) * duration_ms
+        chunk_duration_ms = int(chunk_duration_ms)
+
+        num_chunks = math.ceil(duration_ms / chunk_duration_ms)
+        chunks: List[Tuple[str, int]] = []
+
         for i in range(num_chunks):
-            start_time = i * chunk_duration
-            end_time = (i + 1) * chunk_duration
-            chunk = audio[start_time:end_time]
-            chunk.export(f"{audio_path}_parts/{i}.mp3", format="mp3")
+            start_offset_ms = i * chunk_duration_ms
+            end_offset_ms = (i + 1) * chunk_duration_ms
+            chunk = audio[start_offset_ms:end_offset_ms]
+            export_path = f"{audio_chunk_path}/{i}.mp3"
+            chunk.export(export_path, format="mp3")
+            chunks.append((export_path, start_offset_ms))
 
-            break  # TODO REMOVE ME
+        return chunks
 
     def get_segments_for_chunk(self, chunk_path: str) -> List[TranscriptionSegment]:
         with open(chunk_path, "rb") as f:
